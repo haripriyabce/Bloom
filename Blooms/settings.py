0ff59c4587129f6709fea08722b7cca6ba8f336a
Haripriya K <haripriyakh2019@gmail.com>
2022-10-08 14:46:27 +0530

    add dependencies

--- Blooms/settings.py
+++ Blooms/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['13.230.153.253','localhost','127.0.0.1']
+ALLOWED_HOSTS = ['35.78.96.209','localhost','127.0.0.1']
 
 
 # Application definition
@@ -90,19 +90,19 @@ WSGI_APPLICATION = 'Blooms.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('NAME'),
-        'USER': os.environ.get('USER'),
-        'PASSWORD': os.environ.get('PASSWORD'),
-        'HOST': 'localhost',
+    #   'NAME': os.environ.get('NAME'),
+    #   'USER': os.environ.get('USER'),
+     #   'PASSWORD': os.environ.get('PASSWORD'),
+     #   'HOST': os.environ.get('HOST'),
+     #   'PORT':  os.environ.get('PORT'),
+        'NAME':'myblooms',
+        'USER':'bloomuser',
+        'PASSWORD':'bloompass',
+        'HOST':'localhost',
         'PORT': '5432',
     }
 }
