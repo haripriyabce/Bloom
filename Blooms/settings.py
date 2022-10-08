95b842837a4b05287c3a08bbfabd87f6df2aa5cc
Haripriya K <haripriyakh2019@gmail.com>
2022-10-08 16:27:34 +0530

    dirs removed

--- Blooms/settings.py
+++ Blooms/settings.py
@@ -145,9 +145,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 MEDIA_URL = 'ims/'
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, "static/")
