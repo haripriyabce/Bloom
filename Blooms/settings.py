dcb25970d53c0f5d681ada322f8ee5179403c43b
Haripriya K <haripriyakh2019@gmail.com>
2022-10-08 16:42:47 +0530

    static root

--- Blooms/settings.py
+++ Blooms/settings.py
@@ -144,13 +144,12 @@ USE_TZ = True
 
 
 STATIC_URL = 'static/'
-
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 MEDIA_URL = 'ims/'
 if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+    STATIC_ROOT = os.path.join(BASE_DIR, "static_root")
 else:
     STATIC_ROOT=''
     
