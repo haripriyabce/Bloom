bd3208f780906fd03929152360cca4e04c369435
Haripriya K <haripriyakh2019@gmail.com>
2022-10-09 07:15:01 +0530

    key

--- Blooms/settings.py
+++ Blooms/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-+uhymlv45lcdhk3bp%4ugk9xj07#@u0ci_7)++e6p=x&14mcw!'
+SECRET_KEY = 'django-insecure-+uhymlv45lcdhk3bp%4ugk9xj07#@u0ci_7)++e6p=x&14mcw!'
 
-SECRET_KEY = os.environ.get('SECRET_KEY')
+#SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 if not DEBUG:
     ALLOWED_HOSTS = ['35.78.96.209','localhost','127.0.0.1']
