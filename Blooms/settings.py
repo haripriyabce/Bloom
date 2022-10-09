ff3911a45d6ee1ea52c8010ce6493ec0e1cff593
Haripriya K <haripriyakh2019@gmail.com>
2022-10-10 01:56:04 +0530

    template change

--- Blooms/settings.py
+++ Blooms/settings.py
@@ -73,7 +73,7 @@ ROOT_URLCONF = 'Blooms.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'templates')],
+        'DIRS': [os.path.join(BASE_DIR,'../templates').replace('\\','/')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
